cdf03f46b81232c8b0ca6c7b649d06bfd4611aa4
otaiandrey-maker <otaiandrey@gmail.com>
2026-09-09 17:04:36 -0300

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.post("/chat", async (req, res) => {
     }
 
     const response = await ai.models.generateContent({
-      model: "gemini-3.7-flash",
+     model: "gemini-3.8-flash",
       contents: mensagem,
       config: {
         systemInstruction:
